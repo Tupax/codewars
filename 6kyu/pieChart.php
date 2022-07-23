8afcf1970307cfe81e6373cc8ef2f5065b00b917
tupax <tupaxmaro@gmail.com>
2022-07-23 12:20:46 -0300

    Done. Json,round

--- 6kyu/pieChart.php
+++ 6kyu/pieChart.php
@@ -4,15 +4,17 @@
 // The goal of this kata is to return a JSON containing each key with it corresponding angle to two decimal places when necessary.
 // Example
 
-//     Parameter {"Hausa": 4, "Yoruba" : 5, "Igbo" : 6, "Efik" : 1, "Edo" : 4} should return a JSON like {"Hausa":72,"Yoruba":90,"Igbo":108,"Efik":18,"Edo":72}
-//     Parameter {"English": 4, "Polish" : 12, "Russian" : 10, "Spanish" : 2} should return a JSON like {"English":51.43,"Polish":154.29,"Russian":128.57,"Spanish":25.71}
-//     Parameter {"Android": 500, "iOS" : 270, "Microsoft" : 230} should return a JSON like {"Android":180,"iOS":97.2,"Microsoft":82.8}
-
-
 
 function pieChart($obj)
 {
-    $json = '{"key":value}';
+    $data = json_decode($obj,true);
+    $total = array_sum($data);
+    foreach ($data as $key => $value) {
+        $data[$key] = round((((int)($value) * 360)/$total),2);
+    }
+    
+    $json = json_encode($data);
+ 
 	  return $json;
 }
 
