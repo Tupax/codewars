ee809cac6e11c01199fc7c8b3f9dffcc671bd81b
tupax <tupaxmaro@gmail.com>
2022-07-27 21:50:22 -0300

    array_search,using method

---
+++ 5kyu/getTheLoop.php
@@ -0,0 +1,27 @@
+<?php
+
+// You are given a node that is the beginning of a linked list. This list always contains a tail and a loop. Your objective is to determine the length of the loop.
+// For example in the following picture the tail's size is 3 and the loop size is 12:
+
+
+# Use the `next' method to get the following node.
+// node.next
+
+// Use the Node::getNext() instance method to get the following node.
+
+// $node->getNext();
+// Note: do NOT mutate the nodes!
+
+
+function loop_size(Node $n): int {
+  $a = [];
+  while (!in_array($n, $a, true)) {
+    array_push($a, $n);
+    $n = $n->getNext();
+  }
+  return count($a) - array_search($n, $a, true);
+}
+  // var_dump(loop_size(Node::createChain(0, 1)));
+
+
+  // Cant do test because i don't have access to createChain function
